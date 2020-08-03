f6a86f04ea58d108682bba2913c3623533a13b32
Devansh Sharma <47019195+1devansh@users.noreply.github.com>
2020-08-03 19:37:22 +0530

    Delete credentials.py

--- credentials.py
+++
@@ -1,7 +0,0 @@
-API_KEY = "nwiP5dPacV57sK7oAY0fcbNSn"
-API_SECRET_KEY = "KYApaWT3p8HxViAAnDknuOFL4206kVzKsbJbcKjt7VA7amWgyw"
-
-# After the step above, you will be redirected to your app's page.
-# Create an access token under the the "Your access token" section
-ACCESS_TOEKN = "1134035574728212480-6yHfWVtBG6egtlLqYeS8Uuvx86RTYN"
-ACCESS_TOKEN_SECRET = "ZVkN6QXkfFrx5GEyHcKp87Jj1mL2AxuAXmzoofSxhQGEp"
